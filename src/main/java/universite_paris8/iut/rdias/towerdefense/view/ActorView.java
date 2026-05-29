279e8fc76ec73f1864540629da0049043542557e
Raphaël DIAS <rdias@iut.univ-paris8.fr>
2026-05-29 10:22:07 +0200

    Modification du sprite archer

--- src/main/java/universite_paris8/iut/rdias/towerdefense/view/ActorView.java
+++
@@ -1,7 +0,0 @@
-package universite_paris8.iut.rdias.towerdefense.view;
-
-public class ActorView {
-    public double x;
-    
-
-}
--- src/main/java/universite_paris8/iut/rdias/towerdefense/view/TowerView.java
+++ src/main/java/universite_paris8/iut/rdias/towerdefense/view/TowerView.java
@@ -8,11 +8,11 @@ import universite_paris8.iut.rdias.towerdefense.model.Soldier;
 public class TowerView {
     private Tower tower;
     private ImageView image;
-    private static Image imageViking1 = new Image(GroundView.class.getResourceAsStream("/universite_paris8/iut/rdias/towerdefense/sprite/tile_vikingSoldier1.png"));
+    private static Image spriteArcherTower = new Image(GroundView.class.getResourceAsStream("/universite_paris8/iut/rdias/towerdefense/sprite/tower_archer/Sprite_TowerArcherNiv1.pngit "));
 
     public TowerView(Tower tTower) {
         this.tower = tTower;
-        this.image = new ImageView(imageViking1);
+        this.image = new ImageView(spriteArcherTower);
         this.image.layoutXProperty().bind(this.tower.getXProperty().multiply(16).add(-16/2));
         this.image.layoutYProperty().bind(this.tower.getYProperty().multiply(16).add(-16/2));
     }
