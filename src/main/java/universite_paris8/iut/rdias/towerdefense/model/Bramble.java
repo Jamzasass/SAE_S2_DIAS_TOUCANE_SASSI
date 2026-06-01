1fb8f71c25bb09f6c720bd56a012779a964262b7
Raphaël DIAS <rdias@iut.univ-paris8.fr>
2026-06-01 11:08:08 +0200

    Ajout de la class Bramble

--- src/main/java/universite_paris8/iut/rdias/towerdefense/model/Bramble.java
+++ src/main/java/universite_paris8/iut/rdias/towerdefense/model/Bramble.java
@@ -4,13 +4,42 @@ package universite_paris8.iut.rdias.towerdefense.model;
 
 public class Bramble extends Tower{
 
-    public Bramble(Environnement env, int tHp, int tDmg, int tId, double tRange, double tX, double tY, double tSpeedAttack, int cost) {
-        super(env, tHp, tDmg, tId, tRange, tX, tY, tSpeedAttack, cost);
+    private static final double slowFactor = 0.5;
+    private static final int slowDuration =  30;
+
+    public Bramble(Environnement env, int brambleId, double brambleX, double brambleY, double tSpeedAttack, int cost) {
+        super(env, 0, 0, brambleId, 0, brambleX, brambleY, 1.0, 50);
     }
+
+    @Override
     public void act(Environnement env) {
-        System.out.println("acting ...");
+        for (Enemy e : env.getEnemies()) {
+            if (onTile(e)) {
+                e.applySlow(slowFactor, slowDuration);
+            }
+        }
     }
+
+    private boolean onTile(Enemy e){
+        int enemyX = (int) e.getX();
+        int enemyY = (int) e.getY();
+        int brambleX = (int) getX();
+        int brambleY = (int) getY();
+        return enemyX == brambleX && enemyY == brambleY;
+    }
+
+    @Override
+    public boolean isLiving(){
+        return true;
+    }
+
+    @Override
     public void upgrade() {
-        System.out.println("upgrading ...");
+        System.out.println("pas d'upgrade");
+    }
+
+    @Override
+    public void takeDamage(int amount){
+        System.out.println("Indestructible");
     }
 }
