5ed91c06353e7989a94e7310b25605e0f8ad74da
Raphaël DIAS <rdias@iut.univ-paris8.fr>
2026-06-01 11:08:47 +0200

    Ajout de la class SorcererTower

--- src/main/java/universite_paris8/iut/rdias/towerdefense/model/SorcererTower.java
+++ src/main/java/universite_paris8/iut/rdias/towerdefense/model/SorcererTower.java
@@ -1,20 +1,66 @@
 package universite_paris8.iut.rdias.towerdefense.model;
 
 public class SorcererTower extends Tower {
+    private static final int hplvl1 = 300;
+    private static final int hplvl2 = 400;
+    private static final int dmgPerSeclvl1 = 10;
+    private static final int dmgPerSeclvl2 = 15;
+    private static final int zoneDurationlvl1 = 3;
+    private static final int zoneDurationlvl2 = 4;
+    private static final double speedAttacklvl1 = 0.2;
+    private static final double speedAttacklvl2 = 0.35;
+
     private int dmgPerSec;
     private int zoneDuration;
+    private int zoneFramesLeft;
+    private int dmgCounter;
 
-    public SorcererTower(Environnement env, int tHp, int tDmg, int tId, double tRange, double tX, double tY, double tSpeedAttack, int sTdmgPerSec, int sCost, int sTZoneDuration, int sDmgPersec) {
-        super(env, tHp, tDmg, tId, tRange, tX, tY, tSpeedAttack, sCost);
-        this.dmgPerSec = sTdmgPerSec;
-        this.zoneDuration = sTZoneDuration;
+    public SorcererTower(Environnement env, int sorcererId, double sorcererX, double sorcererY) {
+        super(env, hplvl1, 0, sorcererId, 3.0, sorcererX, sorcererY, speedAttacklvl1, 500);
+        this.dmgPerSec = dmgPerSeclvl1;
+        this.zoneDuration = zoneDurationlvl1;
+        this.zoneFramesLeft = 0;
+        this.dmgCounter = 0;
     }
 
     @Override
     public void act(Environnement env) {
-        System.out.println("attackkckezfkojkergojikpezfjvcghk");
+        tick();
+        if (zoneFramesLeft > 0) {
+            dmgCounter++;               //Applique les dégats toutes les sec.
+            if (dmgCounter >= 60) {
+                applyDamageInZone(env);
+                dmgCounter = 0;
+            }
+            zoneFramesLeft--;
+            if (zoneFramesLeft == 0) {
+                resetCooldown(60);  // Une fois que la zone est terminée, il démarre un cooldown
+            }
+        } else if (canAct()) {          // Si pas de zone et que le cooldown soit ok, alors il peut recréer une zone.
+            zoneFramesLeft = zoneDuration * 60;
+            dmgCounter = 0;
+        }
     }
+
+    private void applyDamageInZone(Environnement env) {
+        for (Enemy e : env.getEnemies()) {
+            if (e.isLiving()) {
+                double range = Math.hypot(e.getX() - getX(), e.getY() - getY());
+                if (range <= getRange()) {
+                    e.takeDamage(dmgPerSec);
+                }
+            }
+        }
+    }
+
+    @Override
     public void upgrade() {
-        System.out.println("upgrading ...");
+        if (getLevel() < 2) {
+            setHp(hplvl2);
+            setSpeedAct(speedAttacklvl2);
+            dmgPerSec = dmgPerSeclvl2;
+            zoneDuration = zoneDurationlvl2;
+            lvlUp();
+        }
     }
 }
