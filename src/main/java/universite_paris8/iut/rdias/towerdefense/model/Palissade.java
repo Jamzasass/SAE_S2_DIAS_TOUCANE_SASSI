8d4a36281d926ffb9eb6c319bf31b85a660d5894
Raphaël DIAS <rdias@iut.univ-paris8.fr>
2026-06-01 13:54:15 +0200

    Ajout de la classe Palissade

--- src/main/java/universite_paris8/iut/rdias/towerdefense/model/Palissade.java
+++ src/main/java/universite_paris8/iut/rdias/towerdefense/model/Palissade.java
@@ -1,11 +1,31 @@
-//package universite_paris8.iut.rdias.towerdefense.model;
-//
-//public class Palissade extends Tower {
-//
-//    private static final int hplvl1 = 150;
-//    private static final int hplvl2 = 225;
-//    private static final int dmglvl1 = 40;
-//    private static final int dmglvl2 = 60;
-//    private static final double speedAttacklvl1 = 1.0;
-//    private static final double speedAttacklvl2 = 1.8;
-//}
+package universite_paris8.iut.rdias.towerdefense.model;
+
+public class Palissade extends Tower {
+
+    private static final int hplvl1 = 200;
+    private static final int hplvl2 = 300;
+
+    public Palissade(Environnement env, int palissadeId, double palissadeX, double palissadeY) {
+        super(env, hplvl1, 0, palissadeId, 0, palissadeX, palissadeY, 1.0, 300);
+    }
+
+//    public void takeBelierDamage(int amount) {
+//        super.takeDamage(amount);
+//    }
+
+    @Override
+    public void act(Environnement env) {
+    }
+
+    @Override
+    public void takeDamage(int amount) {
+    }
+
+    @Override
+    public void upgrade() {
+        if (getLevel() < 2) {
+            setHp(hplvl2);
+            lvlUp();
+        }
+    }
+}
