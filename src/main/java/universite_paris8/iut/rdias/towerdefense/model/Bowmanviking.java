dca239f04dcb20cc43ca33a7790b69a2c495dd4b
Hamza SASSI <hsassi@iut.univ-paris8.fr>
2026-06-02 11:57:08 +0200

    Creation des classes enemy

---
+++ src/main/java/universite_paris8/iut/rdias/towerdefense/model/Bowmanviking.java
@@ -0,0 +1,12 @@
+package universite_paris8.iut.rdias.towerdefense.model;
+
+public class Bowmanviking extends Enemy{
+
+    public Bowmanviking(Environnement env,int eId, double eX, double eY) {
+        super(env, 50, 20, eId, 4, eX, eY, 0.10, 25);
+    }
+
+    public void act(){
+        System.out.println("A l'attaque");
+    }
+}
