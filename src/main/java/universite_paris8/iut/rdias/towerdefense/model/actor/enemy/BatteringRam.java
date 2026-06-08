0f875cabe89837bfae44884ec9bd0bd18a411f07
Raphaël DIAS <rdias@iut.univ-paris8.fr>
2026-06-08 11:17:03 +0200

    Correction code pour les classes des troupes ennemies v1

--- src/main/java/universite_paris8/iut/rdias/towerdefense/model/actor/enemy/BatteringRam.java
+++ src/main/java/universite_paris8/iut/rdias/towerdefense/model/actor/enemy/BatteringRam.java
@@ -21,8 +21,9 @@ public class BatteringRam extends Enemy {
             setyCible((int) target.getY());
             this.move();
 
-            if (calculDistanceFromEnemy(target) < this.getRange()) {
-                target.takeDamage(this.getDmg());
+            if (calculDistanceFromEnemy(target) < this.getRange() && canAct()) {
+                ((Palissade) target).takeBatteringDamage(this.getDmg());
+                resetCooldown(180);
             }
         } else {
             this.move();
@@ -32,12 +33,12 @@ public class BatteringRam extends Enemy {
     public void searchtarget() {
         Tower closeTarget = null;
         double minRange = Double.MAX_VALUE;
-        for (Tower t : getEnvironnement().getTowers()) {
-            if (t.isLiving() && (t instanceof Palissade)) {
-                int range = calculDistanceFromEnemy(t); // calcul distance via pythagore
-                if (range<= minRange) {//(range <= getRange() && range < minRange) {
+        for (Tower p : getEnvironnement().getTowers()) {
+            if (p.isLiving() && (p instanceof Palissade)) {
+                int range = calculDistanceFromEnemy(p); // calcul distance via pythagore
+                if (range<= minRange) { //(range <= getRange() && range < minRange) {
                     minRange = range;
-                    closeTarget = t;
+                    closeTarget = p;
                 }
             }
         }
