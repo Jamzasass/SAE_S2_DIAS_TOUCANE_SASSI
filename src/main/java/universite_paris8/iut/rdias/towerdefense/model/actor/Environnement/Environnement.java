c6bcf81eb46919520f2b5468d08c4cf6c452582f
Raphaël DIAS <rdias@iut.univ-paris8.fr>
2026-06-19 11:29:59 +0200

    Modification de la bourse du joueur

--- src/main/java/universite_paris8/iut/rdias/towerdefense/model/actor/Environnement/Environnement.java
+++ src/main/java/universite_paris8/iut/rdias/towerdefense/model/actor/Environnement/Environnement.java
@@ -61,7 +61,7 @@ public class Environnement {
         this.animations = FXCollections.observableArrayList();
         this.actorsDying = new ArrayList<>();
         this.effectsDying = new ArrayList<>();
-        this.balance = new SimpleIntegerProperty(1000);
+        this.balance = new SimpleIntegerProperty(350);
         this.waveIndex = new SimpleIntegerProperty(1);
         this.settings = new Settings();
         this.wave = new Wave(this, waveIndex.get());
